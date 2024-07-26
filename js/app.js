66cd7d2ec9e1fc2625fccf9738215eaeb828f1ed
William Del Rosario <82073842+WillRos06@users.noreply.github.com>
2024-07-26 18:37:59 -0400

    This commit add the validation for the email input and button

--- js/app.js
+++ js/app.js
@@ -1,34 +1,49 @@
 document.addEventListener('DOMContentLoaded', function(){
+    
+    const email = {
+        email:'',
+        asunto: '',
+        mensaje: '',
+    }
+
     // Seleccionar los elemetnso de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
 
     // Asignar eventos
-    inputEmail.addEventListener('blur', validar);
+    inputEmail.addEventListener('input', validar);
 
-    inputAsunto.addEventListener('blur', validar);
+    inputAsunto.addEventListener('input', validar);
 
-    inputMensaje.addEventListener('blur', validar);
+    inputMensaje.addEventListener('input', validar);
 
     function validar(e){
-        console.log(e.target.parentElement);
-
         if(e.target.value.trim() === ''){
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        } else {
-            console.log('Tiene datos');
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
         }
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return
+        }
+        limpiarAlerta(e.target.parentElement);   
+
+        // Asignar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase(); 
+        
+        // Comprobar el objeto email 
+        comprobarEmail();
     }
 
     function mostrarAlerta(mensaje, referencia){
-        // Comprueba si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta){
-            alerta.remove();
-        }
+        limpiarAlerta(referencia);
 
         // Generar alerta en html 
         const error = document.createElement('P');
@@ -37,5 +52,30 @@ document.addEventListener('DOMContentLoaded', function(){
         
         // Inyectar el error al formulario
         referencia.appendChild(error);
+    }
+
+    function limpiarAlerta(referencia){
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+    function comprobarEmail(){
+        console.log(email);
+        if(Object.values(email).includes('')){
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        } 
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
     }
 });
