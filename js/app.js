3c126390b2a1cbfe7e6eec511f82ad651232d8dc
William Del Rosario <82073842+WillRos06@users.noreply.github.com>
2024-07-26 12:54:31 -0400

    This commit add the functionality of base validation

--- js/app.js
+++ js/app.js
@@ -3,6 +3,31 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
-    
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+
+    inputAsunto.addEventListener('blur', validar);
+
+    inputMensaje.addEventListener('blur', validar);
+
+    function validar(e){
+        if(e.target.value.trim() === ''){
+            mostrarAlerta();
+        } else {
+            console.log('Tiene datos');
+        }
+    }
+
+    function mostrarAlerta(){
+        // Generar alerta en html 
+        const error = document.createElement('P');
+        error.textContent = 'Hubo un error...';
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+        
+        // Inyectar el error al formulario
+        formulario.appendChild(error);
+    }
 });
